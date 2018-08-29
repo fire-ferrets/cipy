ca37995dddb8a709980f5cd760651d9b478239f5
Aaron <39431903+whzup@users.noreply.github.com>
2018-08-29 09:53:14 +0200

    Add Alphabet class (#5)
    Added an Alphabet class (data structure) that behaves like a dictionary but in both
    directions keys->values and values->keys. If called by a print function (or similar) it returns the string value of the initialization alphabet.

---
+++ cipy/alphabet/alphabet.py
@@ -0,0 +1,63 @@
+"""
+The Alphabet class provides a general framework to embed alphabets
+
+It takes a string of the chosen alphabet as attribute. This provides
+the encryption and decryption functions for the ciphers with a performance
+boost.
+"""
+
+import itertools
+
+class Alphabet():
+    def __init__(self, alphabet):
+        """
+        Initialise an Alphabet
+
+        Attributes
+        ----------
+        alphabet : str
+            The alphabet string
+        """
+        self.alphabet = alphabet
+        self._length = len(alphabet)
+        self._numbers_to_alphabet = dict(enumerate(self.alphabet))
+        self._alphabet_to_numbers = dict((v, k) for k,v in self._numbers_to_alphabet.items())
+
+    def __repr__(self):
+        return self.alphabet
+
+    def __len__(self):
+        return self._length
+
+    def __len_hint__(self):
+        return self._length
+
+    def __getitem__(self, key):
+        if key in self._numbers_to_alphabet:
+            return self._numbers_to_alphabet[key]
+        elif key in self._alphabet_to_numbers:
+            return self._alphabet_to_numbers[key]
+
+    def __missing__(self,key, value):
+        raise KeyError("Key not found in Alphabet")
+
+    def __iter__(self):
+        iterable = itertools.chain(
+                self._numbers_to_alphabet.keys(),
+                self._alphabet_to_numbers.keys())
+        return iterable
+
+    def __reversed__(self):
+        iterable = itertoo.chain(
+                reversed(self._numbers_to_alphabet.keys()),
+                reversed(self._alphabet_to_numbers.keys())
+                )
+        return iterable
+
+    def __contains__(self, item):
+        if item in self._numbers_to_alphabet.keys():
+            return True
+        elif item in self._alphabet_to_numbers.keys():
+            return True
+        else:
+            return False
