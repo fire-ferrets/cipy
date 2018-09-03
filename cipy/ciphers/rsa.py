c491902698cb87b7684c3468df6f4c01ede2e7ea
fairu1024 <worstmailaddress@gmail.com>
2018-09-03 22:21:55 +0200

    Implement encrypt and decrypt function for ASCII alphabet Add gcd and check if prime functions in preparation for key generation

--- cipy/ciphers/rsa.py
+++ cipy/ciphers/rsa.py
@@ -2,7 +2,45 @@
 RSA cipher
 
 Allows you to encrypt and decrypt text with the RSA cipher
+At the moment only allows to encrypt ASCII text
 """
+import math
+
+
+def _gcd(a: int, b: int) -> int:
+    """
+    Calculate the greatest common denominator for two given numbers
+
+    Parameters
+    ----------
+    a : int
+        the first number you want to test
+    b : int
+        the second number you want to test
+    """
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def _check_if_prime(num: int) -> bool:
+    """
+    Check whether a natural number is a prime
+
+    Parameters
+    ----------
+    num : int
+        the number you want to test
+    """
+    if num < 2 or num % 2 == 0:
+        return False
+    if num == 2:
+        return True
+    for x in xrange(3, int(math.sqrt(num)), 2):
+        if num % n == 0:
+            return False
+    return True
+
 
 def encrypt(msg: str, n: int, e: int) -> str:
     """
@@ -10,14 +48,15 @@ def encrypt(msg: str, n: int, e: int) -> str:
 
     Parameters
     ----------
-    msg: str
+    msg : str
         the message you want to encrypt
-    n: int
+    n : int
         the modulus n of your keypair
-    e: int
+    e : int
         the public exponent e of your public key
     """
-    pass
+    secret_msg = " ".join([(ord(x) ** e) % n for x in msg])
+    return secret_msg
 
 
 def decrypt(msg: str, n: int, d: int) -> str:
@@ -26,14 +65,15 @@ def decrypt(msg: str, n: int, d: int) -> str:
 
     Parameters
     ----------
-    msg: str
+    msg : str
         the message you want to encrypt
-    n: int
+    n : int
         the modulus n of your keypair
-    d: int
+    d : int
         the secret exponent d of your private key
     """
-    pass
+    plain_msg = "".join(chr((x ** d) % n) for x in msg.split(" "))
+    return secret_msg
 
 
 def gen_keypair() -> dict:
